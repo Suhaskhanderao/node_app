ac8cdd1e2d6f5527e9d184c3aa70e60ee121b6a2
suhas <suhaskhanderao051@gmail.com>
2025-01-20 19:30:36 +0000

    add

--- myfile.java
+++ myfile.java
@@ -2,6 +2,6 @@ class myfile
 {
     public static void main(String[] args)
     {
-        System.out.println("welcome from java12");
+        System.out.println("welcome from ...java12");
     }
 }
